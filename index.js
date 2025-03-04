f17e5a5b5f2aa5ab060e25cb75bb221e372a71c9
goddestshark <135029716+goddestshark@users.noreply.github.com>
2025-03-04 23:10:18 +1000

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["👤 Managing Operations", "🎮 Playing ARP",];
+const statusMessages = ["👤 Managing Operations", "🎮 Playing ARP", "🥽 ARP Oversight",];
 const statusTypes = [ 'online', 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
